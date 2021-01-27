ade0b2abd9e57a8715951251b91e873762271266
wathigo <wathigosimon@gmail.com>
2021-01-27 07:27:37 -0500

    Add helpers

--- src/app/_helpers/auth.guard.ts
+++ src/app/_helpers/auth.guard.ts
@@ -11,10 +11,10 @@ export class AuthGuard implements CanActivate {
     ) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        const currentUser = this.authenticationService.currentUserValue;
+        const currentUser = this.authenticationService.currentUserValue; 
         if (currentUser) {
             // check if route is restricted by role
-            if (route.data.roles && route.data.roles.indexOf(currentUser.role) === -1) {
+            if (route.data.roles && route.data.roles.indexOf(currentUser.is_doctor) === false) {
                 // role not authorised so redirect to home page
                 this.router.navigate(['/']);
                 return false;
